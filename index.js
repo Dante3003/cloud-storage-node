027562c509c8eccd401e38a6f2b75e3941eafb97
Nozimjon <nozimjonjuraev3003@gmail.com>
2022-05-13 07:54:13 +0500

    REFACTOR: add test route

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('config');
-const cors = require('./middlware/corsMiddlware') 
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const cors = require("./middlware/corsMiddlware");
+const fileUpload = require("express-fileupload");
 
-const authRoutes = require('./routes/auth');
-const fileRoutes = require('./routes/file');
+const authRoutes = require("./routes/auth");
+const fileRoutes = require("./routes/file");
 
 const app = express();
-const PORT = config.get('serverPort')
+const PORT = config.get("serverPort");
+const HOST = config.get("serverHost");
 
 app.use(fileUpload({}));
 app.use(express.json());
 app.use(cors);
-app.use('/api/auth', authRoutes);
-app.use('/api/files', fileRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/files", fileRoutes);
 
 const startServer = () => {
   try {
-    mongoose.connect(config.get('dbUrl'))
-    app.listen(PORT, () => {
-      console.log(`server started at port ${PORT}`);
+    mongoose.connect(config.get("dbUrl"));
+    app.listen(PORT, HOST, () => {
+      console.log(`server started at ${HOST}:${PORT}`);
     });
   } catch (err) {
     console.log(err);
   }
-}
+};
 
-startServer()
+startServer();
--- routes/auth.js
+++ routes/auth.js
@@ -1,48 +1,54 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 // const { check, validationResult } = require('express-validator');
-const User = require('../models/User');
-const authMiddleware = require('../middlware/authMiddleware')
-const fileService = require('../services/fileService');
-const File = require('../models/File');
+const User = require("../models/User");
+const authMiddleware = require("../middlware/authMiddleware");
+const fileService = require("../services/fileService");
+const File = require("../models/File");
 
+router.get("/", async (req, res) => {
+  console.log("Worked!!!");
+  res.status(200).send("Worked!!!!");
+});
 
-router.post('/registration', async (req, res) => {
+router.post("/registration", async (req, res) => {
   try {
     const { name, email, password } = await req.body;
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
-    const condidate = await User.findOne({ email })
+    const condidate = await User.findOne({ email });
 
     if (condidate) {
-      return res.status(400).json({ message: `User with email ${email} already exists` })
+      return res
+        .status(400)
+        .json({ message: `User with email ${email} already exists` });
     }
 
-    const user = new User({ name: name, email: email, password: hashPassword })
-    await user.save()
-    await fileService.createDir(new File({user: user.id, name: ''}))
+    const user = new User({ name: name, email: email, password: hashPassword });
+    await user.save();
+    await fileService.createDir(new File({ user: user.id, name: "" }));
     return res.json({ user: { name: user.name, email: user.email } });
-
   } catch (err) {
     console.log(err);
-    res.status(500).send({ message: 'Server error' })
+    res.status(500).send({ message: "Server error" });
   }
-})
+});
 
-
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = await req.body;
-    const currentUser = await User.findOne({ email })
+    const currentUser = await User.findOne({ email });
     if (!currentUser) {
-      return res.status(404).json({ message: 'User not found' })
+      return res.status(404).json({ message: "User not found" });
     }
     const validPassword = await bcrypt.compare(password, currentUser.password);
-    !validPassword && res.send('Wrong password')
-    const token = jwt.sign({ id: currentUser.id }, config.get('secretKey'), { expiresIn: '5h' });
+    !validPassword && res.send("Wrong password");
+    const token = jwt.sign({ id: currentUser.id }, config.get("secretKey"), {
+      expiresIn: "5h",
+    });
     return res.json({
       token,
       user: {
@@ -52,17 +58,19 @@ router.post('/login', async (req, res) => {
         diskSpace: currentUser.diskSpace,
         usedSpace: currentUser.usedSpace,
         avatar: currentUser.avatar,
-      }
-    })
+      },
+    });
   } catch (err) {
-    res.status(500).json({ message: 'Server error' })
+    res.status(500).json({ message: "Server error" });
   }
-})
+});
 
-router.get('', authMiddleware,  async (req, res) => {
+router.get("", authMiddleware, async (req, res) => {
   try {
-    const user = await User.findOne({ _id: req.user.id })
-    const token = jwt.sign({ id: user.id }, config.get('secretKey'), { expiresIn: '5h' });
+    const user = await User.findOne({ _id: req.user.id });
+    const token = jwt.sign({ id: user.id }, config.get("secretKey"), {
+      expiresIn: "5h",
+    });
     return res.json({
       token,
       user: {
@@ -72,11 +80,11 @@ router.get('', authMiddleware,  async (req, res) => {
         diskSpace: user.diskSpace,
         usedSpace: user.usedSpace,
         avatar: user.avatar,
-      }
-    })
+      },
+    });
   } catch (err) {
     console.log(err);
   }
-})
+});
 
 module.exports = router;
